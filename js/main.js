c5ee90e9f9dc2fe778a07ed8672625186801b336
tabbara <abbara@gmail.com>
2014-10-15 00:45:42 +0200

    comma fix

--- js/main.js
+++ js/main.js
@@ -19,16 +19,15 @@ var app = {
             self.showAlert('Store Initialized', 'Info');
         });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+    },
     
     showAlert: function (message, title) {
-    if (navigator.notification) {
-        navigator.notification.alert(message, null, title, 'OK');
-    } else {
-        alert(title ? (title + ": " + message) : message);
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
     }
-    
-},
 
 };
 
