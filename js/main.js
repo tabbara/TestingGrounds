306ca53060e35b8ae0a3c93be8a3c6a304f3a146
tabbara <abbara@gmail.com>
2014-10-15 00:39:16 +0200

    added a notification

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var app = {
 
-    findByName: function() {
+    findByName: function () {
         console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
             var l = employees.length;
@@ -14,9 +14,21 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
+        var self = this;
+        this.store = new MemoryStore(function() {
+            self.showAlert('Store Initialized', 'Info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
+    
+    showAlert: function (message, title) {
+    if (navigator.notification) {
+        navigator.notification.alert(message, null, title, 'OK');
+    } else {
+        alert(title ? (title + ": " + message) : message);
+    }
+    
+},
 
 };
 
